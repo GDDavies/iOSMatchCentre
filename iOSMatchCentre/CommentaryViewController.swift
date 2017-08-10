73573ef6bb55d17ecc281327a5e02867f5075248
George <gdaviesdev@gmail.com>
2017-08-10 23:31:38 +0100

    tableview shows commentary and time

--- iOSMatchCentre/CommentaryViewController.swift
+++ iOSMatchCentre/CommentaryViewController.swift
@@ -9,27 +9,64 @@
 import UIKit
 
 class CommentaryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var eventTypes = [String]()
+    var eventTimes = [String]()
+    var eventHeadings = [String]()
+    var eventDescriptions = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        getData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    /*
+    func getData() {
+        let url = URL(string: "https://feeds.tribehive.co.uk/DigitalStadiumServer/opta?pageType=matchCommentary&value=803294&v=2")
+        
+        let task = URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            DispatchQueue.main.async(execute: {
+                self.populateData(data!)
+            })
+        })
+        task.resume()
+    }
+    
+    func populateData(_ matchCommentaryData: Data) {
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: matchCommentaryData, options: []) as! NSArray
+            
+            for i in 0..<json.count {
+                if let matchEvent = json[i] as? NSDictionary {
+                    print(matchEvent["type"] as! String)
+                    print(matchEvent["time"] as! String)
+                    print(matchEvent["heading"] as! String)
+                    print(matchEvent["description"] as! String)
+                    
+                    eventTypes.append(matchEvent["type"] as! String)
+                    eventTimes.append(matchEvent["time"] as! String)
+                    eventHeadings.append(matchEvent["heading"] as! String)
+                    eventDescriptions.append(matchEvent["description"] as! String)
+                }
+            }
+            // Reload tableView data once all JSON data is loaded
+            tableView.reloadData()
+        } catch {
+            // Error popup
+            print("Error fetching data")
+        }
+    }
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     // MARK: - TableView Methods
 
@@ -38,12 +75,12 @@ class CommentaryViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return eventDescriptions.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CommentaryCell", for: indexPath)
-        cell.textLabel?.text = "Test"
+        cell.textLabel?.text = eventTimes[indexPath.row] + " " + eventDescriptions[indexPath.row]
         return cell
     }
 }
--- iOSMatchCentre/ViewController.swift
+++ iOSMatchCentre/ViewController.swift
@@ -27,51 +27,14 @@ class ViewController: UIViewController {
         }
     }
     
-    var eventTypes = [String]()
-    var eventTimes = [String]()
-    var eventHeadings = [String]()
-    var eventDescriptions = [String]()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        getData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func getData() {
-        let url = URL(string: "https://feeds.tribehive.co.uk/DigitalStadiumServer/opta?pageType=matchCommentary&value=803294&v=2")
-        
-        let task = URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
-            DispatchQueue.main.async(execute: {
-                self.populateData(data!)
-            })
-        })
-        task.resume()
-    }
-
-    func populateData(_ matchCommentaryData: Data) {
-        
-        do {
-            let json = try JSONSerialization.jsonObject(with: matchCommentaryData, options: []) as! NSArray
-            
-            for i in 0..<json.count {
-                if let matchEvent = json[i] as? NSDictionary {
-                    print(matchEvent["type"] as! String)
-                    print(matchEvent["time"] as! String)
-                    print(matchEvent["heading"] as! String)
-                    print(matchEvent["description"] as! String)
-                }
-            }
-            
-        } catch {
-            // Error popup
-            print("Error fetching data")
-        }
     }
 }
 
