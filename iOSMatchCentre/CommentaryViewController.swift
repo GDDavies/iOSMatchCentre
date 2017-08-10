3056b5f0b28247df66e620111905c28d39f0bb85
George <gdaviesdev@gmail.com>
2017-08-10 23:14:26 +0100

    tableview setup

---
+++ iOSMatchCentre/CommentaryViewController.swift
@@ -0,0 +1,49 @@
+//
+//  CommentaryViewController.swift
+//  iOSMatchCentre
+//
+//  Created by George Davies on 10/08/2017.
+//  Copyright © 2017 George Davies. All rights reserved.
+//
+
+import UIKit
+
+class CommentaryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - TableView Methods
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CommentaryCell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+}
