9351227981dbb6cfc549b347f74014435c4d7323
George <gdaviesdev@gmail.com>
2017-08-15 13:43:08 +0100

    issue #3

--- iOSMatchCentre/ViewController.swift
+++ iOSMatchCentre/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var containerViewB: UIView!
     @IBOutlet weak var segmentController: UISegmentedControl!
     
+    var loadingOverlay: UIView?
+    var loadingDataIndicator: UIActivityIndicatorView?
+    
     @IBAction func segmentedController(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             UIView.animate(withDuration: 0.5, animations: {
@@ -28,6 +31,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.removeOverlay), name: NSNotification.Name(rawValue: matchDataNCKey), object: nil)
+        
         // Do any additional setup after loading the view, typically from a nib.
         self.bringContainerViewAToFront()
         
@@ -38,11 +44,47 @@ class ViewController: UIViewController {
         
         MatchJSONData.sharedInstance.getMatchStatsData()
         CommentaryJSONData.sharedInstance.getMatchCommentaryData()
+        
+        addOverlay()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func addOverlay() {
+        
+        let w = UIScreen.main.bounds.width
+        let h = UIScreen.main.bounds.height
+        
+        loadingDataIndicator = UIActivityIndicatorView(frame: view.frame)
+        loadingDataIndicator?.color = UIColor.black
+        
+        loadingDataIndicator?.center = CGPoint(x: w / 2, y: h / 2)
+        
+        loadingDataIndicator?.startAnimating()
+        
+        
+        loadingOverlay = UIView(frame: view.frame)
+        loadingOverlay?.backgroundColor = UIColor.white
+        
+//        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 50))
+//        label.numberOfLines = 0
+//        label.text = "Loading Data - Please wait..."
+//        label.center = CGPoint(x: w / 2, y: h / 2)
+//        label.textAlignment = NSTextAlignment.center
+
+        loadingOverlay?.addSubview(loadingDataIndicator!)
+        
+        loadingOverlay?.alpha = 1.0
+        view.addSubview(loadingOverlay!)
+    }
+    
+    func removeOverlay() {
+        loadingDataIndicator?.stopAnimating()
+        loadingDataIndicator?.removeFromSuperview()
+        loadingOverlay?.removeFromSuperview()
     }
     
     func bringContainerViewAToFront() {
